f9b14748f4b4d8bedc93e928f51b3ad33ea54e51
SravanS96 <80521517+SravanS96@users.noreply.github.com>
2021-05-26 16:24:05 +0530

    Initial commit

---
+++ src/test/java/com/framework/tests/HomePageTest.java
@@ -0,0 +1,16 @@
+package com.framework.tests;
+
+import com.framework.base.TestBase;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(reports.ExtentListener.class)
+public class HomePageTest extends TestBase {
+
+
+
+    @Test
+    public void TC_01_This_is_Test(){
+
+    }
+}
